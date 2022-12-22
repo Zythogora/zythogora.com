52683ceaea6a39dff481ffdb95949d0d42078b60
Carl-Adrien Mercey <carl@zythogora.com>
2022-12-22 12:55:36 +0100

    feat: add beer reviews to beer page

--- src/business/beer/pages/index.tsx
+++ src/business/beer/pages/index.tsx
@@ -23,6 +23,20 @@ const BeerPage = () => {
   return (
     <Layout title={'Zythogora'}>
       <BeerHeader beerId={parseInt(beerId)} />
+
+      <div className="w-256 p-12 bg-white dark:bg-gray-700 rounded-xl">
+        <div className="flex flex-row gap-x-6 mb-12 pb-12 border-b dark:border-gray-500">
+          <div className="px-8 py-4 rounded-full shadow-md bg-gray-700 text-white dark:bg-gray-300 dark:text-gray-900">
+            My reviews
+          </div>
+          <div className="px-8 py-4 rounded-full shadow-md bg-gray-300 dark:bg-gray-900 dark:text-gray-300">
+            Friend reviews
+          </div>
+          <div className="px-8 py-4 rounded-full shadow-md bg-gray-300 dark:bg-gray-900 dark:text-gray-300">
+            Brewer notes
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
