12c019b38ed77d320cf2980d3ca09055dee3330b
Carl-Adrien Mercey <camercey@gmail.com>
2025-02-24 18:56:48 +0100

    fix: add cascade on user deletion

---
+++ prisma/migrations/20250224184833_cascade_user_deletion/migration.sql
@@ -0,0 +1,9 @@
+BEGIN;
+
+-- DropForeignKey
+ALTER TABLE "public"."users" DROP CONSTRAINT "users_id_fkey";
+
+-- AddForeignKey
+ALTER TABLE "public"."users" ADD CONSTRAINT "users_id_fkey" FOREIGN KEY ("id") REFERENCES "authentication"."users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+COMMIT;
