Zythogora/dev.zythogora.com

feat: add beer creation form

src/app/[locale]/create/beer/schemas.ts
@@ -1,0 +1,27 @@
+import { z } from "zod";
+
+import { zRequiredString } from "@/lib/validator";
+
+export const createBeerSchema = z.object({
+  name: zRequiredString,
+  breweryId: zRequiredString,
+  style: zRequiredString,
+  color: zRequiredString,
+  abv: z
+    .number({ required_error: "form.errors.FIELD_REQUIRED" })
+    .min(0)
+    .max(100),
+  ibu: z.number().min(0).optional(),
+  releasedIn: z
+    .number()
+    .min(1000, {
+      message: "createBeerPage.errors.MIN_CREATION_YEAR_ERROR",
+    })
+    .max(new Date().getFullYear(), {
+      message: "createBeerPage.errors.MAX_CREATION_YEAR_ERROR",
+    })
+    .optional(),
+  description: z.string().optional(),
+});
+
+export type CreateBeerData = z.infer<typeof createBeerSchema>;

src/app/_components/ui/color-select/index.tsx
@@ -2,70 +2,66 @@
 
 import { Command } from "cmdk";
 import { CheckIcon, ChevronDownIcon, SearchIcon } from "lucide-react";
-import { useLocale, useTranslations } from "next-intl";
+import { useTranslations } from "next-intl";
 import { useState } from "react";
 
-import CountryFlag from "@/app/_components/icons/country-flag";
+import PintIcon from "@/app/_components/icons/pint";
 import Button from "@/app/_components/ui/button";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from "@/app/_components/ui/popover";
-import countries from "@/lib/i18n/countries";
 import { cn } from "@/lib/tailwind";
 
-import type { Country } from "@/lib/i18n/countries/types";
+import type { Color } from "@/domain/beers/types";
 import type { getSelectProps } from "@conform-to/react";
 import type { ComponentProps } from "react";
 
-interface CountrySelectProps
+interface ColorSelectProps
   extends Partial<ReturnType<typeof getSelectProps>>,
     Pick<ComponentProps<"input">, "placeholder" | "disabled" | "className"> {
-  onChange?: (value: Country) => void;
+  colors: Color[];
+  onChange?: (value: Color) => void;
   searchPlaceholder?: string;
 }
 
-const CountrySelect = ({
+const ColorSelect = ({
+  colors,
   onChange,
   placeholder,
   searchPlaceholder,
   className,
   ...restProps
-}: CountrySelectProps) => {
+}: ColorSelectProps) => {
   const t = useTranslations();
-  const locale = useLocale();
 
   const [open, setOpen] = useState(false);
-  const [selectedCountry, setSelectedCountry] = useState<Country | null>(null);
-
-  const countryList = Object.entries(countries.getNames(locale))
-    .map(([code, name]) => ({ code, name }))
-    .sort((a, b) => a.name.localeCompare(b.name));
+  const [selectedColor, setSelectedColor] = useState<Color | null>(null);
 
   return (
     <Popover open={open} onOpenChange={setOpen}>
       <PopoverTrigger asChild>
         <Button
           {...restProps}
-          data-slot="country-select-trigger"
+          data-slot="color-select-trigger"
           variant="outline"
           aria-expanded={open}
           role="combobox"
-          value={selectedCountry?.code}
+          value={selectedColor?.id}
           className={cn(
-            "group/country-select-trigger font-medium",
+            "group/color-select-trigger font-medium",
             "justify-between pr-4",
             "hover:bottom-0 hover:before:-bottom-1",
             "aria-invalid:before:bg-destructive",
             className,
           )}
         >
-          {selectedCountry ? (
+          {selectedColor ? (
             <div className="flex min-w-0 flex-row items-center gap-x-3">
-              <CountryFlag country={selectedCountry} size={20} />
+              <PintIcon color={selectedColor} size={20} />
 
-              <p className="truncate">{selectedCountry.name}</p>
+              <p className="truncate">{selectedColor.name}</p>
             </div>
           ) : (
             <span className="text-foreground-muted">{placeholder}</span>
@@ -75,8 +71,8 @@ const CountrySelect = ({
             size={24}
             className={cn(
               "shrink-0 transition-transform duration-300",
-              "group-aria-expanded/country-select-trigger:-scale-y-100",
-              "group-aria-invalid/country-select-trigger:text-destructive",
+              "group-aria-expanded/color-select-trigger:-scale-y-100",
+              "group-aria-invalid/color-select-trigger:text-destructive",
             )}
             aria-hidden="true"
           />
@@ -90,11 +86,12 @@ const CountrySelect = ({
         className="w-full max-w-[calc(100vw-theme(spacing.16))] min-w-[calc(var(--radix-popper-anchor-width)+theme(spacing.1))] p-0"
       >
         <Command
-          data-slot="country-select"
+          data-slot="color-select"
           filter={(_, search, keywords) => {
             if (
               keywords &&
-              keywords.join(" ").toLowerCase().includes(search.toLowerCase())
+              keywords.length > 0 &&
+              keywords[0]!.toLowerCase().includes(search.toLowerCase())
             ) {
               return 1;
             }
@@ -103,7 +100,7 @@ const CountrySelect = ({
           }}
         >
           <div
-            data-slot="country-select-input-container"
+            data-slot="color-select-input-container"
             className={cn(
               "flex items-center gap-x-3 rounded-t border-b-2 px-5 py-4 drop-shadow",
               "bg-background dark:bg-stone-700",
@@ -112,7 +109,7 @@ const CountrySelect = ({
             <SearchIcon className="size-5 shrink-0" />
 
             <Command.Input
-              data-slot="country-select-input"
+              data-slot="color-select-input"
               placeholder={searchPlaceholder}
               className={cn(
                 "flex w-full rounded outline-hidden",
@@ -124,27 +121,27 @@ const CountrySelect = ({
           </div>
 
           <Command.List
-            data-slot="country-select-list"
+            data-slot="color-select-list"
             className={cn(
               "scroll-py-1 overflow-x-hidden overflow-y-auto rounded",
-              "max-h-52 md:max-h-72",
+              "max-h-48",
               "bg-background dark:bg-stone-700",
               "*:[&[cmdk-list-sizer]]:p-2",
             )}
           >
             <Command.Empty className={cn("px-3 py-2", "text-sm md:text-base")}>
-              {t("form.fields.countrySelect.noResult")}
+              {t("form.fields.colorSelect.noResult")}
             </Command.Empty>
 
-            {countryList.map((country) => (
+            {colors.map((color) => (
               <Command.Item
-                data-slot="country-select-item"
-                key={country.code}
-                value={country.code}
-                keywords={[country.code, country.name]}
+                data-slot="color-select-item"
+                key={color.id}
+                value={color.id}
+                keywords={[color.name]}
                 onSelect={() => {
-                  onChange?.(country);
-                  setSelectedCountry(country);
+                  onChange?.(color);
+                  setSelectedColor(color);
                   setOpen(false);
                 }}
                 className={cn(
@@ -154,11 +151,11 @@ const CountrySelect = ({
                   "data-[disabled=true]:bg-background-muted data-[disabled=true]:pointer-events-none data-[disabled=true]:cursor-not-allowed",
                 )}
               >
-                <CountryFlag country={country} size={20} />
+                <PintIcon color={color} size={20} />
 
-                <p className="truncate">{country.name}</p>
+                <p className="truncate">{color.name}</p>
 
-                {selectedCountry?.code === country.code ? (
+                {selectedColor?.id === color.id ? (
                   <CheckIcon size={16} className="ml-auto" />
                 ) : null}
               </Command.Item>
@@ -170,4 +167,4 @@ const CountrySelect = ({
   );
 };
 
-export default CountrySelect;
+export default ColorSelect;

Zythogora/zythogora.com

src/app/[locale]/(forms)/create/beer/schemas.ts
@@ -1,0 +1,27 @@
+import { z } from "zod";
+
+import { zRequiredString } from "@/lib/validator";
+
+export const createBeerSchema = z.object({
+  name: zRequiredString,
+  breweryId: zRequiredString,
+  styleId: zRequiredString,
+  colorId: zRequiredString,
+  abv: z
+    .number({ required_error: "form.errors.FIELD_REQUIRED" })
+    .min(0)
+    .max(100),
+  ibu: z.number().min(0).optional(),
+  releasedIn: z
+    .number()
+    .min(1000, {
+      message: "createBeerPage.errors.MIN_CREATION_YEAR_ERROR",
+    })
+    .max(new Date().getFullYear(), {
+      message: "createBeerPage.errors.MAX_CREATION_YEAR_ERROR",
+    })
+    .optional(),
+  description: z.string().optional(),
+});
+
+export type CreateBeerData = z.infer<typeof createBeerSchema>;

src/app/_components/ui/color-select/index.tsx
@@ -2,70 +2,66 @@
 
 import { Command } from "cmdk";
 import { CheckIcon, ChevronDownIcon, SearchIcon } from "lucide-react";
-import { useLocale, useTranslations } from "next-intl";
+import { useTranslations } from "next-intl";
 import { useState } from "react";
 
-import CountryFlag from "@/app/_components/icons/country-flag";
+import ColoredPintIcon from "@/app/_components/icons/colored-pint";
 import Button from "@/app/_components/ui/button";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from "@/app/_components/ui/popover";
-import countries from "@/lib/i18n/countries";
 import { cn } from "@/lib/tailwind";
 
-import type { Country } from "@/lib/i18n/countries/types";
+import type { Color } from "@/domain/beers/types";
 import type { getSelectProps } from "@conform-to/react";
 import type { ComponentProps } from "react";
 
-interface CountrySelectProps
+interface ColorSelectProps
   extends Partial<ReturnType<typeof getSelectProps>>,
     Pick<ComponentProps<"input">, "placeholder" | "disabled" | "className"> {
-  onChange?: (value: Country) => void;
+  colors: Color[];
+  onChange?: (value: Color) => void;
   searchPlaceholder?: string;
 }
 
-const CountrySelect = ({
+const ColorSelect = ({
+  colors,
   onChange,
   placeholder,
   searchPlaceholder,
   className,
   ...restProps
-}: CountrySelectProps) => {
+}: ColorSelectProps) => {
   const t = useTranslations();
-  const locale = useLocale();
 
   const [open, setOpen] = useState(false);
-  const [selectedCountry, setSelectedCountry] = useState<Country | null>(null);
-
-  const countryList = Object.entries(countries.getNames(locale))
-    .map(([code, name]) => ({ code, name }))
-    .sort((a, b) => a.name.localeCompare(b.name));
+  const [selectedColor, setSelectedColor] = useState<Color | null>(null);
 
   return (
     <Popover open={open} onOpenChange={setOpen}>
       <PopoverTrigger asChild>
         <Button
           {...restProps}
-          data-slot="country-select-trigger"
+          data-slot="color-select-trigger"
           variant="outline"
           aria-expanded={open}
           role="combobox"
-          value={selectedCountry?.code}
+          value={selectedColor?.id}
           className={cn(
-            "group/country-select-trigger font-medium",
+            "group/color-select-trigger font-medium",
             "justify-between pr-4",
             "hover:bottom-0 hover:before:-bottom-1",
             "aria-invalid:before:bg-destructive",
             className,
           )}
         >
-          {selectedCountry ? (
+          {selectedColor ? (
             <div className="flex min-w-0 flex-row items-center gap-x-3">
-              <CountryFlag country={selectedCountry} size={20} />
+              <ColoredPintIcon color={selectedColor} size={20} />
 
-              <p className="truncate">{selectedCountry.name}</p>
+              <p className="truncate">{selectedColor.name}</p>
             </div>
           ) : (
             <span className="text-foreground-muted">{placeholder}</span>
@@ -75,8 +71,8 @@ const CountrySelect = ({
             size={24}
             className={cn(
               "shrink-0 transition-transform duration-300",
-              "group-aria-expanded/country-select-trigger:-scale-y-100",
-              "group-aria-invalid/country-select-trigger:text-destructive",
+              "group-aria-expanded/color-select-trigger:-scale-y-100",
+              "group-aria-invalid/color-select-trigger:text-destructive",
             )}
             aria-hidden="true"
           />
@@ -90,11 +86,12 @@ const CountrySelect = ({
         className="w-full max-w-[calc(100vw-theme(spacing.16))] min-w-[calc(var(--radix-popper-anchor-width)+theme(spacing.1))] p-0"
       >
         <Command
-          data-slot="country-select"
+          data-slot="color-select"
           filter={(_, search, keywords) => {
             if (
               keywords &&
-              keywords.join(" ").toLowerCase().includes(search.toLowerCase())
+              keywords.length > 0 &&
+              keywords[0]!.toLowerCase().includes(search.toLowerCase())
             ) {
               return 1;
             }
@@ -103,7 +100,7 @@ const CountrySelect = ({
           }}
         >
           <div
-            data-slot="country-select-input-container"
+            data-slot="color-select-input-container"
             className={cn(
               "flex items-center gap-x-3 rounded-t border-b-2 px-5 py-4 drop-shadow",
               "bg-background dark:bg-stone-700",
@@ -112,7 +109,7 @@ const CountrySelect = ({
             <SearchIcon className="size-5 shrink-0" />
 
             <Command.Input
-              data-slot="country-select-input"
+              data-slot="color-select-input"
               placeholder={searchPlaceholder}
               className={cn(
                 "flex w-full rounded outline-hidden",
@@ -124,27 +121,27 @@ const CountrySelect = ({
           </div>
 
           <Command.List
-            data-slot="country-select-list"
+            data-slot="color-select-list"
             className={cn(
               "scroll-py-1 overflow-x-hidden overflow-y-auto rounded",
-              "max-h-52 md:max-h-72",
+              "max-h-48",
               "bg-background dark:bg-stone-700",
               "*:[&[cmdk-list-sizer]]:p-2",
             )}
           >
             <Command.Empty className={cn("px-3 py-2", "text-sm md:text-base")}>
-              {t("form.fields.countrySelect.noResult")}
+              {t("form.fields.colorSelect.noResult")}
             </Command.Empty>
 
-            {countryList.map((country) => (
+            {colors.map((color) => (
               <Command.Item
-                data-slot="country-select-item"
-                key={country.code}
-                value={country.code}
-                keywords={[country.code, country.name]}
+                data-slot="color-select-item"
+                key={color.id}
+                value={color.id}
+                keywords={[color.name]}
                 onSelect={() => {
-                  onChange?.(country);
-                  setSelectedCountry(country);
+                  onChange?.(color);
+                  setSelectedColor(color);
                   setOpen(false);
                 }}
                 className={cn(
@@ -154,11 +151,11 @@ const CountrySelect = ({
                   "data-[disabled=true]:bg-background-muted data-[disabled=true]:pointer-events-none data-[disabled=true]:cursor-not-allowed",
                 )}
               >
-                <CountryFlag country={country} size={20} />
+                <ColoredPintIcon color={color} size={20} />
 
-                <p className="truncate">{country.name}</p>
+                <p className="truncate">{color.name}</p>
 
-                {selectedCountry?.code === country.code ? (
+                {selectedColor?.id === color.id ? (
                   <CheckIcon size={16} className="ml-auto" />
                 ) : null}
               </Command.Item>
@@ -170,4 +167,4 @@ const CountrySelect = ({
   );
 };
 
-export default CountrySelect;
+export default ColorSelect;