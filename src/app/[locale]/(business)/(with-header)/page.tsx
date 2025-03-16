0515258085989ad163d94c451178dd6a7a0b7b81
Carl-Adrien Mercey <camercey@gmail.com>
2025-03-17 00:01:19 +0100

    feat: add St Patrick's theme

--- src/app/[locale]/(business)/(with-header)/page.tsx
+++ src/app/[locale]/(business)/(with-header)/page.tsx
@@ -2,6 +2,8 @@ import { getTranslations } from "next-intl/server";
 import { Suspense } from "react";
 
 import HasVerifiedEmail from "@/app/[locale]/(business)/(with-header)/_components/has-verified-email";
+import StPatrickHatIcon from "@/app/_components/icons/st-patrick-hat";
+import { cn } from "@/lib/tailwind";
 
 const HomePage = async () => {
   const t = await getTranslations();
@@ -12,15 +14,33 @@ const HomePage = async () => {
         <HasVerifiedEmail />
       </Suspense>
 
-      <div className="flex flex-col items-center justify-center">
-        <h1 className="text-center text-4xl font-bold">
+      <div
+        className={cn(
+          "flex flex-col items-center justify-center gap-y-8",
+          "pt-16 md:pt-8",
+        )}
+      >
+        <h1 className="relative text-center text-4xl font-bold">
           {t.rich("home.title", {
             br: () => <br />,
             highlight: (chunks) => (
               <span className="text-primary text-5xl">{chunks}</span>
             ),
           })}
+
+          <StPatrickHatIcon
+            size={40}
+            className="absolute -right-6 bottom-[calc(1ex-2px)] size-16 rotate-15"
+          />
         </h1>
+
+        <p className="max-w-md text-center text-2xl">
+          {t.rich("home.stPatrickWelcomeMessage", {
+            highlight: (chunks) => (
+              <span className="text-primary font-bold">{chunks}</span>
+            ),
+          })}
+        </p>
       </div>
     </>
   );
